891cffec63237ce71f76ceca36b3a2028981948a
Kenichi Yorozu <rozken@gmail.com>
2012-06-26 23:28:50 +0900

    added test on help page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+
   describe "Home Page" do
     it "should have the conetent 'Sample App'" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
     end
   end
+  
+  describe "Help Page" do
+  	it "should have the content 'Hep'" do
+  		visit '/static_pages/help'
+  		page.should have_content('Help')
+  	end
+  end
+  
 end
